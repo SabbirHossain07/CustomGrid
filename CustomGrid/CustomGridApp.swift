cb46f44e00b83d1590334eeefedb870d995300dd
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-10-25 19:41:28 +0600

    Initial Commit

---
+++ CustomGrid/CustomGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomGridApp.swift
+//  CustomGrid
+//
+//  Created by Sopnil Sohan on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
